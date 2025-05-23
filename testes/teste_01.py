4c2abfa2a986ad08cd34b28047f11d4c1a1b0e40
pamella666 <Pamellag464@gmail.com>
2025-05-22 21:09:36 -0300

    Update teste_01.py

--- testes/teste_01.py
+++ testes/teste_01.py
@@ -1,1 +1,20 @@
-#teste01
+import unittest
+
+# Função a ser testada
+def soma(a, b):
+    return a + b
+
+# Classe de teste
+class TestSoma(unittest.TestCase):
+    def test_soma_positivos(self):
+        self.assertEqual(soma(1, 2), 3)
+
+    def test_soma_negativos(self):
+        self.assertEqual(soma(-1, -2), -3)
+
+    def test_soma_zero(self):
+        self.assertEqual(soma(0, 0), 0)
+
+# Executar os testes
+if __name__ == '__main__':
+    unittest.main(argv=['first-arg-is-ignored'], exit=False)
